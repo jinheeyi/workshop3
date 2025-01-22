ed90ffeed1a5fe58a6e597386683c6495e8f5d68
jinheeyi <jyi111@aucklanduni.ac.nz>
2025-01-22 17:33:33 +1300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -24,7 +24,10 @@ function draw(){
   image(img[3], width/2, height/2, width/2, height/2);
   filter(BLUR,5)
 
-  fill(255); // Set text color to white
-  text("My Beautiful Day", width/2, height / 2); // centered text
-}
+  fill(255); // white text
+  text("i have finished my day... goodbye, and see you soon", width / 2, height / 2); // centered text
 
+  textSize(10); // setting font size
+  textAlign(CENTER, CENTER); // center text
+  textFont("Courier New"); // courier new text font
+}
